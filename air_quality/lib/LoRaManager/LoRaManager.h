9c380f03eeaebc7539f05c0d9561b42a1154031c
DonatFortini <donat.fortini00@gmail.com>
2025-04-02 16:06:01 +0200

    adjustments

--- air_quality/lib/LoRaManager/LoRaManager.h
+++ air_quality/lib/LoRaManager/LoRaManager.h
@@ -1,34 +1,34 @@
-#ifndef LORA_MANA_MANAGER_H
+#ifndef LORA_MANAGER_H
 #define LORA_MANAGER_H
 
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-class LoRaManager
-{
+class LoRaManager {
 private:
-    SoftwareSerial *loraSerial;
-    long previousTTN;
-    unsigned long uplinkInterval;
-    bool receiveCallback;
-    bool getDataStatus;
-    bool networkJoinedStatus;
+  SoftwareSerial *loraSerial;
+  long previousTTN;
+  unsigned long uplinkInterval;
+  bool receiveCallback;
+  bool getDataStatus;
+  bool networkJoinedStatus;
 
-    String inputString;
-    bool stringComplete;
+  String inputString;
+  bool stringComplete;
 
-    char rxbuff[128];
-    uint8_t rxbuff_index;
+  char rxbuff[128];
+  uint8_t rxbuff_index;
 
-    void processLoRaData();
+  void processLoRaData();
 
 public:
-    LoRaManager(byte rxPin, byte txPin);
-    void begin();
-    void handleLoRaMessages();
-    void sendAirQualityData(uint16_t pm25, uint16_t pm10, int aqiValue, uint8_t alertState);
-    bool isNetworkJoined();
-    void processSerialCommands();
+  LoRaManager(byte rxPin, byte txPin);
+  void begin();
+  void handleLoRaMessages();
+  void sendAirQualityData(uint16_t pm25, uint16_t pm10, int aqiValue,
+                          uint8_t alertState);
+  bool isNetworkJoined();
+  void processSerialCommands();
 };
 
 #endif // LORA_MANAGER_H
